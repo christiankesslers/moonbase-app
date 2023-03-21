2dc3b19f325cee947af7992b2a7f5b294d0fbd7a
devofficer <myeyesontech@gmail.com>
2023-03-21 16:03:00 -0700

    feat: discover skeleton done

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,3 +1,4 @@
+import { BriefcaseIcon } from '@heroicons/react/24/outline';
 import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
 import CogIcon from '@heroicons/react/24/solid/CogIcon';
 import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
@@ -24,6 +25,15 @@ export const items = [
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Discover',
+    path: '/discover',
+    icon: (
+      <SvgIcon fontSize="small">
+        <BriefcaseIcon />
       </SvgIcon>
     )
   },
