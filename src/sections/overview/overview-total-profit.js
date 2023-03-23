40267cda962632ed9a641179c2d244113ef4e46e
Roy Hodge Jr <118377684+modelomaker812@users.noreply.github.com>
2023-03-23 01:17:21 -0500

    Update overview-total-profit.js
    changed title

--- src/sections/overview/overview-total-profit.js
+++ src/sections/overview/overview-total-profit.js
@@ -19,7 +19,7 @@ export const OverviewTotalProfit = (props) => {
               color="text.secondary"
               variant="overline"
             >
-              Total Profit
+              Total Proposals
             </Typography>
             <Typography variant="h4">
               {value}
