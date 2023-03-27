8dfeb7f7ed12281ccf58f33324d6c8644d2059f3
SpaceRanch Dev <91358513+SpaceRanchDev@users.noreply.github.com>
2023-03-27 08:49:10 -0700

    Update config.js

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -30,7 +30,7 @@ export const items = [
   },
    {
     title: 'Organization',
-    path: '/discover',
+    path: '/dao/test.sputnikv2.testnet',
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
